139f7336f09e338572c4ef7521bf37e7b9b8879b
karimunnisa12345 <arshiyajyothi@gmail.com>
2022-08-08 15:02:18 +0530

    pushing the githubstdDemo project into github - first commit

---
+++ GithubStdDemo/src/Demopackage/DemoClass.java
@@ -0,0 +1,11 @@
+package Demopackage;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is first github program demo");
+	}
+
+}
